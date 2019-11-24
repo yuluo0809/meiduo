504327cd83e347a2f3bc6e5006cb6f71a7d7ba1e
yuluo <yuluo0809@icloud.com>
2019-11-24 19:35:04 +0800

    2019-11-24 User模型追加email_active字段并迁移

--- meiduo_mall/meiduo_mall/apps/oauth/urls.py
+++ meiduo_mall/meiduo_mall/apps/oauth/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     # 拼接QQ登录url
     url(r'^qq/authorization/$', views.QQAuthURLView.as_view()),
+    # QQ登录成功回调处理
+    url(r'^oauth_callback/$', views.QQAuthView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/settings/developing.py
+++ meiduo_mall/meiduo_mall/settings/developing.py
@@ -205,3 +205,8 @@ AUTH_USER_MODEL = 'users.User'
 
 # 修改登录路由
 LOGIN_URL = '/login/'
+
+# QQ登录配置项
+QQ_CLIENT_ID = '101518219'
+QQ_CLIENT_SECRET = '418d84ebdc7241efb79536886ae95224'
+QQ_REDIRECT_URI = 'http://www.meiduo.site:8000/oauth_callback'
